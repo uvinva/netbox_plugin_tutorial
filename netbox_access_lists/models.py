3ee32debaebbefa6408afc62973780f09b7ccbd8
uvinva <uvinva@gmail.com>
2022-04-12 21:17:50 +0500

    finish step 5

--- netbox_access_lists/models.py
+++ netbox_access_lists/models.py
@@ -1,28 +1,47 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.urls import reverse
 from netbox.models import NetBoxModel
 from utilities.choices import ChoiceSet
+
+class ActionChoices(ChoiceSet):
+    key = 'AccessListRule.action'
+
+    CHOICES = [
+        ('permit', 'Permit', 'green'),
+        ('deny', 'Deny', 'red'),
+        ('reject', 'Reject (Reset)', 'orange'),
+    ]
+
+class ProtocolChoices(ChoiceSet):
+
+    CHOICES = [
+        ('tcp', 'TCP', 'blue'),
+        ('udp', 'UDP', 'orange'),
+        ('icmp', 'ICMP', 'purple'),
+    ]
+
 
 class AccessList(NetBoxModel):
     name = models.CharField(
         max_length=100
     )
     default_action = models.CharField(
-        max_length=30
+        max_length=30,
+	choices=ActionChoices
     )
     comments = models.TextField(
         blank=True
     )
     class Meta:
         ordering = ('name',)
-    # AccessList
-    default_action = models.CharField(
-        max_length=30,
-        choices=ActionChoices
-    )
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('plugins:netbox_access_lists:accesslist', args=[self.pk])
+
     def get_default_action_color(self):
         return ActionChoices.colors.get(self.default_action)
 
@@ -62,7 +81,8 @@ class AccessListRule(NetBoxModel):
         null=True
     )
     action = models.CharField(
-        max_length=30
+        max_length=30,
+	choices=ActionChoices
     )
     description = models.CharField(
         max_length=500,
@@ -71,40 +91,21 @@ class AccessListRule(NetBoxModel):
     class Meta:
         ordering = ('access_list', 'index')
         unique_together = ('access_list', 'index')
-    
-    # AccessListRule
-    action = models.CharField(
-        max_length=30,
-        choices=ActionChoices
-    )
 
     protocol = models.CharField(
         max_length=30,
         choices=ProtocolChoices,
         blank=True
-    )    
+    )
+
     def __str__(self):
         return f'{self.access_list}: Rule {self.index}'
+
+    def  get_absolute_url ( self ):
+         return  reverse ( 'plugins: netbox_access_lists :accesslistrule' , args = [ self.pk ] )
 
     def get_protocol_color(self):
         return ProtocolChoices.colors.get(self.protocol)
 
     def get_action_color(self):
         return ActionChoices.colors.get(self.action)
-
-class ActionChoices(ChoiceSet):
-    key = 'AccessListRule.action'
-
-    CHOICES = [
-        ('permit', 'Permit', 'green'),
-        ('deny', 'Deny', 'red'),
-        ('reject', 'Reject (Reset)', 'orange'),
-    ]
-
-class ProtocolChoices(ChoiceSet):
-
-    CHOICES = [
-        ('tcp', 'TCP', 'blue'),
-        ('udp', 'UDP', 'orange'),
-        ('icmp', 'ICMP', 'purple'),
-    ]
